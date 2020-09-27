fd23cbdd903c799619821c2cb4aa8b97b1745a5c
Fuza322 <vanya.osipik322@gmail.com>
2020-09-27 13:05:20 +0300

    Task1

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {MessageDataPropsType} from "./HW1";
+import style from './Message.module.css'
+
+
+
+function Message(props: MessageDataPropsType) {
+    return (
+        <div className={style.divMessageContainer}>
+            <div className={style.divImageMessageSize}>
+                <img className={style.imageMessageSize} src={props.avatar} alt="User's Photo"/>
+            </div>
+            <div className={style.divMessageInfo}>
+                <div className={style.divMessageUserName}>
+                    {props.name}
+                </div>
+                <div className={style.divMessageText}>
+                    {props.message}
+                </div>
+            </div>
+            <div>
+                {props.time}
+            </div>
+        </div>
+    );
+}
+
+export default Message;
