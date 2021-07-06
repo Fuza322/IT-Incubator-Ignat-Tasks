de16604df1f37b504063d0712bb724d3efdc1090
Fuza322 <vanya.osipik322@gmail.com>
2021-07-06 19:59:44 +0300

    finish task9

--- src/p2-homeworks/h9/clock/Clock.tsx
+++ src/p2-homeworks/h9/clock/Clock.tsx
@@ -1,31 +1,33 @@
 import React, {useState} from "react"
-import {SuperButton} from "../h4/common/c2-SuperButton/SuperButton"
+import moment from "moment"
+import {SuperButton} from "../../h4/common/c2-SuperButton/SuperButton"
+import style from "./Clock.module.css"
 
 export function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<string>(moment().format('LTS'))
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(moment().format('LTS'))
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = "Time" // fix with date
-    const stringDate = "Date" // fix with date
+    const stringTime = date
+    const stringDate = moment().format("MMM Do YY")
 
     return (
         <div>
@@ -40,8 +42,14 @@ export function Clock() {
                     {stringDate}
                 </div>
             )}
-            <SuperButton onClick={start}>start</SuperButton>
-            <SuperButton onClick={stop}>stop</SuperButton>
+            <SuperButton
+                onClick={start}
+                className={style.buttonSize}
+            >start</SuperButton>
+            <SuperButton
+                onClick={stop}
+                className={style.buttonSize}
+            >stop</SuperButton>
         </div>
     )
 }
